f0bea922094b3ed82b28e95e345448d4b8ec816f
Rodrigo_LaptopI3_600U <rodrigogutierrez273@gmail.com>
2022-06-09 12:19:53 -0300

    configurando react 18

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import react from "react";
+import { createRoot } from 'react-dom/client';
+import App from "./App";
+import './app.scss';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App/>);
